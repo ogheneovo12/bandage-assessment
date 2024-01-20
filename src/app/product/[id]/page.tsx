4dfa60dac293279156c1a394694b33820fe9d962
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2024-01-20 06:45:50 +0100

    added comment to product  page  approach

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,12 +1,17 @@
+import { ApiService } from "@/redux/services";
 import { ProductServiceEndpoints } from "@/redux/services/products.service";
 import { store } from "@/redux/store";
+import { Box, Typography } from "@mui/material";
 
+// USED SERVER SIDE HERE TO SHOW HOW TO USE SERVER SIDE DATA FETCHING
+// CLIENT SIDE CAN BE USED AS WELL
+// DOWNSIDE OF SERVER SIDE IS VERCEL HAS A 10 SECOND TIMEOUT LIMIT FOR EDGE FUNCTIONS FOR FREE TIER
 const getProduct = async (productId: string) => {
   try {
     const response = await store
       .dispatch(ProductServiceEndpoints.getProduct.initiate(productId))
       .unwrap();
-
+    store.dispatch(ApiService.util.resetApiState()); //cancel all timers
     return response;
   } catch (err) {}
   return null;
@@ -20,5 +25,14 @@ export default async function ProductPage({
   const product = await getProduct(params.id);
   console.log({ product });
 
-  return <div>{product?.name}</div>;
+  return (
+    <>
+      <header>
+        <h1>Product Page</h1>
+        <Box>
+          <Typography>{product?.name}</Typography>
+        </Box>
+      </header>
+    </>
+  );
 }
